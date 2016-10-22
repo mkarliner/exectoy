96286e745b053acc232b89d692242dcc69ee1b9a
Michael Karliner <mike@modern-industry.com>
2016-10-22 10:56:32 +0100

    NEW: Reset command FIX: Encoder publish string

--- main.py
+++ main.py
@@ -3,12 +3,19 @@ import ssd1306
 import time
 from simple import MQTTClient
 from encoder import Encoder  # or from pyb_encoder import Encoder
-
-
+import network
+import uftpd
+import ujson
+import machine
 
 
 from machine import Pin
 from neopixel import NeoPixel
+
+
+#Network
+
+wlan = network.WLAN(network.STA_IF)
 
 #Neo Pixels
 active_led  = 0
@@ -19,7 +26,7 @@ np = NeoPixel(pin, 12)   # create NeoPixel driver on GPIO0 for 8 pixels
 np[active_led] = (80, 20, 20) # set the first pixel to white
 np.write()              # write data to all pixels
 r, g, b = np[0]         # get first pixel colour
-colour = "red"
+colour = "green"
 # colours = {
 #     "red": (255,0,0),
 #     "green": (0,255,0),
@@ -38,11 +45,19 @@ display = ssd1306.SSD1306_I2C(128, 64, i2c)
 
 display.text("Hello", 1,1)
 display.text("RedSift", 1,20)
+
 display.show()
 
 #Encoder
 
-# e = Encoder(14, 12)  # optional: add pin_mode=Pin.PULL_UP
+menu = [
+    "No menu",
+    "Nada",
+    "nothing",
+    "Reset"
+]
+
+e = Encoder(14, 12, clicks=4)  # optional: add pin_mode=Pin.PULL_UP
 
 def button_cb(p):
     global button_pushed
@@ -56,46 +71,66 @@ button = Pin(0, Pin.IN, Pin.PULL_UP)
 button.irq(trigger=Pin.IRQ_FALLING, handler=button_cb)
 
 
+
+
+
 # Publish test messages e.g. with:
 # mosquitto_pub -t foo_topic -m hello
 
 # Received messages from subscriptions will be delivered to this callback
 def sub_cb(topic, msg):
-    global colour, colours
+    global colour, colours, menu
     
     print((topic, msg))
-    if(topic == b'led_commands'):
+    if(topic == b'led_cmds'):
         col = msg.decode("utf-8")
         if(col in colours):
+            print("Setting color to ", colour)
             colour = col
-    else:
+    elif(topic == b'oled_messages'):
         display.fill(False)
-        display.text(topic, 1,1)
-        display.text(msg, 1,20)
+        disp = ujson.loads(msg)
+        print(disp)
+        for i in range(len(disp)):
+            display.text(disp[i]['str'], disp[i]['x'], disp[i]['y'])       
         display.show()
+    elif(topic == b'menu_cmds'):
+        menu = ujson.loads(msg)
+        print(menu)
+        print(len(menu))
+        #Set up new encoder
+        #e = Encoder(14,12, min_val=0, max_val = len(menu))
+    
+            
     
 
     
 def check_encoder():
     global lastval
     global button_pushed
+    global menu
     
     val = e.value
     if lastval != val:
         lastval = val
-        for x in range(1,120):
-            for y in range(40,50):
-                display.pixel(x,y,0)
-        display.text(str(val), 1,40)
+        display.fill(0)
+        # for x in range(1,120):
+        #     for y in range(40,50):
+        #         display.pixel(x,y,0)
+        if(val > len(menu)-1):
+            display.text(str(val), 1,40)
+        else:
+            display.text(menu[val], 1, 40)
+        #display.text(str(val), 1,40)
         display.show()
         print(val)
     if(button_pushed):
         button_pushed = False
-        return (True,val)
+        return (True,menu[val])
     else:
         return (False,0)
 
-def mqtt_main(server="169.55.61.183"):
+def mqtt_main(server="192.168.10.22"):
     global np
     global active_led
     global colour
@@ -103,57 +138,65 @@ def mqtt_main(server="169.55.61.183"):
     global direction
     
     
-    c = MQTTClient("umqtt_client", server, user="guest", password="guest")
+    c = MQTTClient("umqtt_client2", server, user="guest", password="guest")
     c.set_callback(sub_cb)
     c.connect()
-    c.subscribe(b"led_commands")
+    c.subscribe(b"led_cmds")
     c.subscribe(b'oled_messages')
+    c.subscribe(b'menu_cmds')
     while True:
-        if False:
-            # Blocking wait for message
-            c.wait_msg()
+        # Non-blocking wait for message
+        c.check_msg()
+        # # Rotate leds fast
+        # np[active_led] = (0,0,0)
+        # active_led+=1
+        # if(active_led==12):
+        #     active_led = 0
+        # np[active_led] = colour
+        # np.write()
+        # Has the encoder value changed?
+        if(colour == "red"):
+            rgb = (0,brightness,0)
+        elif(colour == "green"):
+            rgb = (brightness,0,0)
+        elif(colour == "blue"):
+            rgb = (0,0,brightness)
         else:
-            # Non-blocking wait for message
-            c.check_msg()
-            # # Rotate leds fast
-            # np[active_led] = (0,0,0)
+            print(colour)
+        np[active_led] = rgb
+        np.write()
+        brightness = brightness + direction
+        if(brightness >= 255):
+            direction = -1
+            # np[active_led] = (0, 0,0)
             # active_led+=1
             # if(active_led==12):
             #     active_led = 0
-            # np[active_led] = colour
-            # np.write()
-            # Has the encoder value changed?
-            if(colour == "red"):
-                rgb = (brightness,0,0)
-            elif(colour == "green"):
-                rgb = (0,brightness,0)
-            elif(colour == "blue"):
-                rgb = (0,0,brightness)
-            np[active_led] = rgb
-            np.write()
-            brightness = brightness + direction
-            if(brightness >= 255):
-                direction = -1
-                # np[active_led] = (0, 0,0)
-                # active_led+=1
-                # if(active_led==12):
-                #     active_led = 0
-            if(brightness <= 0):
-                direction = 1
-                np[active_led] = (0, 0,0)
-                active_led+=1
-                if(active_led==12):
-                    active_led = 0
-            pushed, enc_value = check_encoder()
-            pushed = False
-            if(pushed == True):
-                print("ENC: ", pushed, enc_value)
-                c.publish('button_pushes', str(enc_value))
-            
-            # Then need to sleep to avoid 100% CPU usage (in a real
-            # app other useful actions would be performed instead)
-            time.sleep_ms(10)
+        if(brightness <= 0):
+            direction = 1
+            np[active_led] = (0, 0,0)
+            active_led+=1
+            if(active_led==12):
+                active_led = 0
+        pushed, enc_value = check_encoder()
+        if(pushed == True):
+            if(enc_value == "Reset"):
+                print("Resetting")
+                machine.reset()
+            print("ENC: ", pushed, enc_value)
+            c.publish('button_pushes', enc_value)
+        
+        # Then need to sleep to avoid 100% CPU usage (in a real
+        # app other useful actions would be performed instead)
+        time.sleep_ms(10)
 
     c.disconnect()
 
+
+
+while(wlan.isconnected() != True):
+    print("Waiting for Wifi")
+    time.sleep_ms(500)
+display.text(wlan.ifconfig()[0], 1,40)
+display.show()
 mqtt_main()
